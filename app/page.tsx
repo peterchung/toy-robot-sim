8aa253d1d30a9552990d4a45df4db768836edd62
peter chung <peter.chung.716@gmail.com>
2024-12-12 13:45:10 -0500

    feat: wrap BoardProvider around children

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
-import Image from "next/image";
 import { SimBoard } from "./components/SimBoard";
 import { CommandButtons } from "./components/CommandButtons";
 import { ReportButton } from "./components/ReportButton";
 import { Instructions } from "./components/Instructions";
+import { BoardProvider } from "./boardContext/BoardContext";
 
 export default function Home() {
   return (
-    <div className="h-screen flex flex-col items-center justify-evenly mt-10">
-      <Instructions/>
-      <SimBoard/>
-      <CommandButtons/>
-      <ReportButton/>
-    </div>
+    <BoardProvider>
+      <div className="h-screen flex flex-col items-center justify-evenly mt-10">
+        <Instructions/>
+        <SimBoard/>
+        <CommandButtons/>
+        <ReportButton/>
+      </div>
+    </BoardProvider>
   );
 }
