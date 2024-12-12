025fe2f9ef0f0e0c146216984f3289e032f19886
peter chung <peter.chung.716@gmail.com>
2024-12-12 13:53:51 -0500

    feat: update simBoard to render only a single Robot

--- app/components/SimBoard.tsx
+++ app/components/SimBoard.tsx
@@ -1,17 +1,28 @@
+'use client'
+
+import { BoardContext } from "../boardContext/BoardContext";
 import { Robot } from "./Robot";
+import { useContext } from 'react'
 
 export const SimBoard = () => {
-  const board = Array(5).fill(Array(5).fill(null));
+  const context = useContext(BoardContext);
+
+  if (!context) {
+    throw new Error ('context is null or undefined')
+  }
+
+  const { state } = context
+  const board = Array(state.boardSize).fill(Array(state.boardSize).fill(null));
 
   return (
    <div>
     <div className="grid grid-cols-5 w-fit">
-      {board.map((row, y) => row.map((_, x) => (
+      {board.map((row, y: number) => row.map((_, x: number) => (
         <div
         key={`${x}-${y}`}
         className="w-20 h-20 flex items-center justify-center border border-gray-300 "
         >
-          <Robot/>      
+          {state.position.x === x && state.position.y === y && <Robot/>}      
         </div>
       ))
     )}
