d16155fdbc78287e71ae54b60b12d4f653c8c2cd
peter chung <peter.chung.716@gmail.com>
2024-12-12 15:08:15 -0500

    refactor: update instruction 1 in instructions arr

--- app/components/Instructions.tsx
+++ app/components/Instructions.tsx
@@ -1,7 +1,7 @@
 import { Accordion, AccordionContent, AccordionTrigger, AccordionItem } from "@/shadcn/ui/accordion"
 
 const listOfInstructions = [
-  "1. Click on any cell to place the robot",
+  "1. Click on any cell to place the robot (note: placing the robot in a new cell will reset the total move count to 0)",
   "2. Click the 'Move' button or press 'Enter' key to move the robot in the direction it's facing",
   "3a. Click the 'Left/Right' buttons or use 'Left/Right' arrow keys to change the robot's direction",
   "3b. Right rotates the direction 90deg clockwise. Left rotates the direction 90deg counter-clockwise",
