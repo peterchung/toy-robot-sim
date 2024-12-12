65d6946180f6f36b3273f916f098374e9ca91eee
peter chung <peter.chung.716@gmail.com>
2024-12-12 10:48:26 -0500

    feat: update ReportButton to open drawer with report details

--- app/components/ReportButton.tsx
+++ app/components/ReportButton.tsx
@@ -1,10 +1,27 @@
 import { Button } from "@/shadcn/ui/button";
+import { Drawer, DrawerTrigger, DrawerContent, DrawerHeader, DrawerTitle, DrawerDescription, DrawerFooter, DrawerClose } from "@/shadcn/ui/drawer";
 
 export const ReportButton = () => {
-  // TODO: add functionality to provide report details onClick (direction, location, total moves, etc.)
   return (
-    <Button variant='secondary' size='lg' className="border border-solid border-cyan-400">
+    <Drawer>
+      <DrawerTrigger asChild>
+      <Button variant='secondary' size='lg' className="border border-solid border-cyan-400">
       Report
     </Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <div className="mx-auto">
+          <DrawerHeader>
+            <DrawerTitle>Robot Report</DrawerTitle>
+            <DrawerDescription>Placeholder for direction, location, total moves</DrawerDescription>
+          </DrawerHeader>
+          <DrawerFooter>
+            <DrawerClose asChild>
+              <Button size='sm'>Close</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </div>
+      </DrawerContent>
+    </Drawer>
   )
 }
