7e9f00ced870081c2ebb400fb67eefb0486fe199
peter chung <peter.chung.716@gmail.com>
2024-12-12 13:32:53 -0500

    feat: add rotate functionality to robot on button click or keydown
    - add missing type definition for listOfButton array

--- app/components/CommandButtons.tsx
+++ app/components/CommandButtons.tsx
@@ -1,9 +1,16 @@
-import { Button } from "@/shadcn/ui/button"
+'use client'
 
-const listOfButtons = [
+import { Button } from "@/shadcn/ui/button"
+import { useContext, useEffect } from 'react'
+import { actions } from "../boardContext/boardConstants"
+import { BoardContext } from "../boardContext/BoardContext"
+import { ButtonConfig } from "@/lib/types"
+
+const listOfButtons: ButtonConfig[] = [
   {
     type: 'Left',
-    action: 'rotate-left',
+    action: actions.rotate_left,
+    keyBind: 'ArrowLeft',
   },
   {
     type: 'Move',
@@ -11,16 +18,45 @@ const listOfButtons = [
   },
   {
     type: 'Right',
-    action: 'rotate-right',
+    action: actions.rotate_right,
+    keyBind: 'ArrowRight',
   }
 ]
 
 export const CommandButtons = () => {
+  const context = useContext(BoardContext);
+
+  if (context === null) {
+    throw new Error('Buttons must be used within BoardProvider')
+  };
+  
+  const { dispatch } = context;
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      listOfButtons.forEach((button) => {
+        if (e.key === button.keyBind) {
+          console.log(`dispatch ${button.type}`)
+          dispatch({ type: actions[button.action]})
+        }
+      })
+    }
+
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {window.removeEventListener('keydown', handleKeyDown)}
+  }, [dispatch])
+  
   return (
     <div className="w-60 flex justify-between">
       {listOfButtons.map((button) => (
       <div key={button.type}>
-        <Button className="w-16">
+        <Button 
+          onClick={() => {
+            console.log(`${button.type} clicked!`)
+            dispatch({ type: button.action })
+          
+          }}
+          className="w-16">
           {button.type}
         </Button>
       </div>
