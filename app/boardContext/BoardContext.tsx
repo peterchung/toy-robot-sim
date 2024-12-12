412100431a07bb2a2676aecfcef4745d12127cb9
peter chung <peter.chung.716@gmail.com>
2024-12-12 13:42:34 -0500

    fix: add 'use client' directive to fix runtime error and render Robot and BoardProvider components

--- app/boardContext/BoardContext.tsx
+++ app/boardContext/BoardContext.tsx
@@ -1,9 +1,11 @@
+'use client'
+
 import { createContext, useReducer} from "react";
 import { boardReducer } from "./boardReducer";
 import { initialState } from "./boardConstants";
 import { BoardContextType } from "@/lib/types";
 
-const BoardContext = createContext<BoardContextType | null>(null);
+export const BoardContext = createContext<BoardContextType | null>(null);
 
 export const BoardProvider = ({ children }: {children: React.ReactNode}) => {
   const [state, dispatch] = useReducer(boardReducer, initialState);
--- app/components/Robot.tsx
+++ app/components/Robot.tsx
@@ -1,12 +1,23 @@
+'use client'
+
 import { Bot, MoveUp } from 'lucide-react';
+import { useContext } from 'react';
+import { BoardContext } from '../boardContext/BoardContext';
 
 export const Robot = () => {
   // TODO: import robot direction state and replace hardcode degree input
+  const context = useContext(BoardContext);
+
+  if (context === null) {
+    throw new Error('context is null or undefined')
+  }
+
+  const { state } = context;
 
   return (
     <div 
     className='flex flex-col items-center justify-center'
-    style={{ transform: `rotate(${90}deg)`}}
+    style={{ transform: `rotate(${state.direction}deg)`}}
     >
       <MoveUp />
       <Bot />
