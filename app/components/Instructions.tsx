c4a85eb84f485be7b49cd4a156e99766e0966033
peter chung <peter.chung.716@gmail.com>
2024-12-12 10:32:37 -0500

    feat: create accordion Instructions component
    - import Accordion component from shadcn

---
+++ app/components/Instructions.tsx
@@ -0,0 +1,27 @@
+import { Accordion, AccordionContent, AccordionTrigger, AccordionItem } from "@/shadcn/ui/accordion"
+
+const listOfInstructions = [
+  "1. Click on any cell to place the robot",
+  "2. Click the 'Move' button or press 'Enter' key to move the robot in the direction it's facing",
+  "3a. Click the 'Left/Right' buttons or use 'Left/Right' arrow keys to change the robot's direction",
+  "3b. Right rotates the direction 90deg clockwise. Left rotates the direction 90deg counter-clockwise",
+]
+
+export const Instructions = () => {
+  return (
+    <div className="rounded-md">
+      <Accordion type='single' collapsible>
+        <AccordionItem value='item-1'>
+        <AccordionTrigger className="font-extrabold text-lg">INSTRUCTIONS:</AccordionTrigger>
+        <AccordionContent>
+          <ul>
+            {listOfInstructions.map((rule, idx) => (
+              <li key={idx}>{rule}</li>
+            ))}
+          </ul>
+        </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import Image from "next/image";
 import { SimBoard } from "./components/SimBoard";
 import { CommandButtons } from "./components/CommandButtons";
 import { ReportButton } from "./components/ReportButton";
+import { Instructions } from "./components/Instructions";
 
 export default function Home() {
   return (
     <div className="h-screen flex flex-col items-center justify-evenly mt-10">
+      <Instructions/>
       <SimBoard/>
       <CommandButtons/>
       <ReportButton/>
