afac4dd98532c74e56480a496bb3003eb4b7fc2d
peter chung <peter.chung.716@gmail.com>
2024-12-12 15:02:03 -0500

    feat: add translateDirection function to convert number to string direction

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,19 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const translateDirection = (direction: number) => {
+  switch (direction) {
+    case 0:
+      return 'North';
+    case 90:
+      return 'East';
+    case 180:
+      return 'South';
+    case 270:
+      return 'West;';
+  }
+};
