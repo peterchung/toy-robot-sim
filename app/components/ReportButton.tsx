094e87dddc059ed6396216dc075279561a6995f7
peter chung <peter.chung.716@gmail.com>
2024-12-12 10:08:17 -0500

    feat: create ReportButton component

---
+++ app/components/ReportButton.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/shadcn/ui/button";
+
+export const ReportButton = () => {
+  // TODO: add functionality to provide report details onClick (direction, location, total moves, etc.)
+  return (
+    <Button variant='secondary' size='lg' className="border border-solid border-cyan-400">
+      Report
+    </Button>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import { SimBoard } from "./components/SimBoard";
 import { CommandButtons } from "./components/CommandButtons";
+import { ReportButton } from "./components/ReportButton";
 
 export default function Home() {
   return (
     <div className="h-screen flex flex-col items-center justify-evenly mt-10">
       <SimBoard/>
       <CommandButtons/>
+      <ReportButton/>
     </div>
   );
 }
